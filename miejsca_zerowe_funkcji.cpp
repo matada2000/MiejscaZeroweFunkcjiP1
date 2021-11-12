b1efdaaa6de586b24c9bd8bb2dd781b7a328cbf0
matada2000 <92324438+matada2000@users.noreply.github.com>
2021-11-12 22:15:04 +0100

    wer 2.0

--- miejsca_zerowe_funkcji.cpp
+++ miejsca_zerowe_funkcji.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
 
-	float a, b, c, mz;
+	float a, b, c, mz_1, mz_2, delta;
 
 	cout << "Podaj a: ", cin >> a;
-    cout << "Podaj b: ", cin >> b;
-    cout << "Podaj c: ", cin >> c;
 
-    if(a == 0 && c == 0){
-        cout << "Funkcja ma nieskonczenie wiele miejsc zerowych" << endl;
-    } else if(a == 0 && c != 0){
-        cout << "Funkcja nie ma miejsc zerowych" << endl;
-    } else{
-        mz = -c / a;
-        cout << "Miejsce zerowe tej funkcji wynosi: " << mz << endl;
-    }
+	if (a == 0) {
+		cout << "a nie moze wynosic 0" << endl;
+	}
+	else {
+		cout << "Podaj b: ", cin >> b;
+		cout << "Podaj c: ", cin >> c;
+
+		delta = (b*b) - (4*a*c);
+
+		if(delta < 0){
+            cout << "Brak miejsc zerowych" << endl;
+		}
+
+		if(delta == 0){
+            mz_1 = -b / (2*a);
+
+            cout << "Miejsce zerowe tej funkcji wynosi: " << mz_1 << endl;
+		}
+
+		if(delta > 0) {
+            mz_1 = (-b - sqrt(delta))/(2*a);
+            mz_2 = (-b + sqrt(delta))/(2*a);
+
+            cout << "Miejsca zerowe tej funkcji wynosza: " << mz_1 << " oraz " << mz_2 << endl;
+		}
+	}
+
 	return 0;
 }
