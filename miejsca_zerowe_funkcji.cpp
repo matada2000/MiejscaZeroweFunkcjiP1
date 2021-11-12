5f864bd5107c7c38a3c27680f9868b35ccb01896
matada2000 <92324438+matada2000@users.noreply.github.com>
2021-11-12 21:56:18 +0100

    wer 1.4

--- miejsca_zerowe_funkcji.cpp
+++ miejsca_zerowe_funkcji.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int main() {
 
-	float a, b, mz;
+	float a, b, c, mz;
 
 	cout << "Podaj a: ", cin >> a;
     cout << "Podaj b: ", cin >> b;
-		
-    if(a == 0 && b == 0){
+    cout << "Podaj c: ", cin >> c;
+
+    if(a == 0 && c == 0){
         cout << "Funkcja ma nieskonczenie wiele miejsc zerowych" << endl;
-    } else if(a == 0 && b != 0){
+    } else if(a == 0 && c != 0){
         cout << "Funkcja nie ma miejsc zerowych" << endl;
     } else{
-        mz = -b / a;
+        mz = -c / a;
         cout << "Miejsce zerowe tej funkcji wynosi: " << mz << endl;
     }
-    
 	return 0;
 }
